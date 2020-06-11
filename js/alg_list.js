17e43190c6acd417c2a4c115bd6d4548846878f1
soncScoot <47670265+soncScoot@users.noreply.github.com>
2020-06-10 20:28:33 -0500

    my favorite ttlls
    notices, etc

--- js/alg_list.js
+++ js/alg_list.js
@@ -1,10 +1,10 @@
 var ttll = {
     "Front Opposite":["(R U' R' U' R U' R') U2 (R U' R' U' R U' R')", "U' (R U R' U R U R' U) (R U' R' U) (R U R' U R U R')", "U (R U' R' U) (R U2 R' U') (R U R')", "(R U' R' U') (R U2 R' U2 R U2) (R' U R U R')", "U' (R2 U2) (R U' R' U R' U2) R2 U (R U R')", "U2 (R U2 R' U' R' U2) (R2 U R2’ U) R", "U2 (R2 U) (R2 U) (R2 U2) R2", "U (R' U' R' U) (R U2 R U2) (R2 U') (R U R)", "R2 U2 (R2 U' R2 U') R2", "U (R U' R' U) R2 U (R' U R' U') R2 U' (R' U2) R'", "(R' U2 R U2) (R U R' U2) (R U R' U') (R' U2) R", "U (R' U') (R2 U' R2 U2) (R U R U2) R'"],
     "Right Opposite":["(y') (R' U2) (R U R' U2) R2 U2 (R' U' R U') R2 U' R", "(y') (R' U R U R' U) R U2 (R' U R U R' U R)", "(y') U' (R' U R U') R' U2 (R U R' U') R", "(y') (R' U R U) (R' U2 R U2 R' U2) (R U' R' U') R", "R2 F2 U2 (R2 U' R2 U') F2 R2", "(y') U2 (R2 U' R2 U') R2 U2 R2", "(y') U2 (R' U2) (R U R U2) (R2 U' R2 U') R'", "(y') U' (R U R U') (R' U2 R' U2) R2 U (R' U' R')", "(y') R2 U2 (R2 U R2 U) R2", "(y') U (R U R U') R2 U2 R U2 (R U R' U') R'", "(y') R2 U2 (R U R U') R' U2 (R U R' U') R' U R2", "(y') U' R U (R2 U R2 U2) (R' U' R' U2) R"],
-	"Double Opposite":[],
-	"Front Bar":[],
-	"Right Bar":[],
-	"Double Bar":[],
+	"Double Opposite":["(R2 U) (R2 D R2 D') (R2 U') (R2 D R2 D')", "U2 (x) (R' U R U') (R' U R U') (R' U R U')", "(R' U' R' U) R2 D' (R U2 R' U2) (R U' R' D) R'", "U' (R U' R' U R U') (R2 F2 R2) U' (R' U' R U) R' F2", "F2 (r2 U' r2 U') (r2 U r2 U) F2", "U (l' U2 l U2) (R U' R' U) (l' U2 l U') R U' (l' U2)", "(F2 U' r2 U') (r2 U r2 U) r2 F2", "F' (R U R' U) R' D' (R U R' D) (R U' R U2) R' F", "(R U' R' U') (R U' R D) (R' U' R D') (R' U R' U2) R U R'", "(R2 D) (R2 U') D' F2 (R2 U' R2 U') F2", "R2 D (r2 U) (r2 U' r2 U') (r2 U) D' R2", "F2 U' R2 U2 (R2 U' R2 U') R2 U F2"],
+	"Front Bar":["(y') R2 (r U' r') F2 (R2 U' R') F R' (F2 R2)", "(y') R2 F2 R F' R U R2 F2 r U r' R2", "R2 y (R U R' U') (R' F) (R2 U' R' U') (R U R' F)", "(R U R' U') (R U' R' U') (R U R D) (R' U' R D') (R' U R' U)", "U' (R U2 R' U') (R U2 R' U') F2 (R U2 R' U2) (l' U2 l)", "R2 U (R U' R' U') (R U R D) (R' U' R D') (R' U2) (R' U2) R2", "(R2 U) (R2 D) (R2 U') (R D' R D) (R' U R U') R (U' D')", "F2 (r2 U') (r' F r' F2) R U' R'", "U R' D' (R U2 R') D R2 U2 (R' U' R U2) (R' U' R U2 R')", "(R U' R' D) (R' U R U') (R' U' R D') (R2 U R U') R2", "U' (R U2) (R' U2) (R' U' R D') (R U' R' D) (R' U2) (R U' R U) R'", "U2 (R U2 R' U) (R U2 R' U) (R U2 R2 D') (R U2 R' D) R"],
+	"Right Bar":["(x') D' (R U' R' D2) (R U R' D')", "(R U2) (R' U2) (R U' R') (U2 R2) D (R' U2 R D') R2", "(R U R' U2) R2 D (R' U R D') R' U2 (R' U R U) R'", "(R U R' U') (R U' R' U') (R U R' U2) R' D' (R U2 R' D) R", "(x) U R U2 (r' U r U) (x') U' R U2 R2", "U (R U2 R' D) (R U R U2) (R' U R' U') (R2 U2)",  "(R' U R' D')", "(R' U R' U') D (R2 U' R U') (R' U R' U) R2 (U D') R2", "(R U2) (R' U2) (R U' R' U2) R2 D (r' U2 r) D' R2", "U' (R2 D) (R2 U') (R2 D') (R2 F2) U2 F2", "(R U' R D) (R' U2 R D') (R' U2 R' U') (R U2) R'", "U (R2 D') (R2 U) (R2 U') (R' U2 R') (r U2 r') D", "U' (R2 U2) (R2 F2) U' (R2 U') (R2 U) F2"],
+	"Double Bar":["D' (r2 U R2 U') (r2 D R2)", "U2 R2 D (R' U2 R D') R2 U2 (R U R' U2) (R U2 R')", "U2 (R U R' U') R U L' (U R' U R) (U' R' U2) L U", "(R U R' U') (R U' R' U') (R U R' U') R' D' (R U R') D R", "U' (R U R' U') R U' (R2 D') (R U' R' D) (R U2 R U') R'", "D' (r U2 r') (R U2 R U) (R2 U' R2 D R2)", "U' (R U2 R' U) (R U R' U') L (U' R U) L' (U' R')", "F (R U R' U') R' F' (R2 U2 R' U2 R U2) R'", "(y') (R' U2 R U2) R' U (R U2 R2 D') (r U2 r') D R2", "(R U2) (R' U2) (R U' R' U2) L' (U R U') L (U2 R')", "U' R2 D (R' U' R D') R2 U (R U' R' U2) (R U2 R')", "(F U' F) (R2 u R' U) (R U' R u') R2 F2"],
 };
 
 var ttllTao = {
@@ -19,16 +19,16 @@ var ttllTao = {
 var tsle = {
     "UN3":["U (R U' R') U' (R U' R') U (R U' R')"],
 	"UN2":["R U' R'", "U' (R U2) (R' U2) (R U2) R'", "R2 D (R' U' R D') R2", "(R U' R' U) (R' U' R U' R' U2 R)", "(R U R' U2) (R U' R' U') R U R'", "(R U R' U') (R U' R')"],
-	"UN1":["U (R U' R' U) (R U2 R')", "(U2 R U2 R') (U R U2 R')", "U' (R U' R' U) (R U R' U') (R U' R')", "(R U' R' U') (R U2 R' U' R U' R')", "(R U R' U2) (R U2 R' U) (R U' R')", "U' R U' (R' U R U') R'", "U' (R U R' U2) (R U2 R')", "U' L' (U R U' R') L", "(R U2 R') U (R U' R') U (R U' R')", "U (R U2 R')", "U' (R U2 R' U) (R U' R')", "U' (R U R' U') R U R'"],
+	"UN1":["U (R U' R' U) (R U2 R')", "(U2 R U2 R') (U R U2 R')", "U' (R U' R' U) (R U R' U') (R U' R')", "(R U' R' U') (R U2 R' U' R U' R')", "(R U R' U2) (R U2 R' U) (R U' R')", "U' R U' (R' U R U') R'", "U' (R U R' U2) (R U2 R')", "U' L' (U R U' R') L / U (R U2 R') U' (R U2 R' U' R U' R')", "(R U2 R') U (R U' R') U (R U' R')", "U (R U2 R')", "U' (R U2 R' U) (R U' R')", "U' (R U R' U') R U R'"],
 	"UN0":["(R U' R' U') (R U R' U) (R U2 R')", "(R U' R' U) (R U R' U R U2 R')", "R U R D (R' U2 R D') R2", "U (R U' R' U) (R U' R' U) (R U2 R')", "U R U' R' U' R U' R' U' R U R' U", "U2 (R U R' U') (R U R' U2) R U R'", "(R U' R' U) R U2 (R' U' R U') R'", "(R U R' U') (R U R' U') R U' R'"],
 	"UCw4":["U (R U' R') U2 (R U R')"],
-	"UCw3":["U' R U R' U R U' R'", "U' L U' R U R' L'", "U R U' R' U' R U R' U R U' R'", "U2 R U R' U' R U' R' U2 R U' R'", "R' D' r U2 r' D R", "R' U2 R U R' U R U' R U R'"],
-	"UCw2":["R U' R' U' R U R' U2 R U R'", "U R U2 R' U2 R U R' U2 R U R'", "R U R' U R U' R' U' R U' R'", "R U' R' U R U2 R' U' R U R' U R U R'", "U2 R U' R' U2 R U R' U R U' R'", "R U R' U2 R U R' U R U' R'", "U2 F' R U R' U' R' F R", "U2 R U R'", "U R U' R' U R U' R' U2 R U R'", "U' R U R' U' R U' R' U R U R'", "R U2 R' U' R U R' U2 R U' R'", "R U' R' U2 R U R' U2 R U R'"],
+	"UCw3":["U' R U R' U R U' R'", "U' L U' R U R' L' / R U R' U R U R' U R U' R' U' R U2 R'", "U R U' R' U' R U R' U R U' R'", "U2 R U R' U' R U' R' U2 R U' R'", "R' D' r U2 r' D R", "R' U2 R U R' U R U' R U R'"],
+	"UCw2":["R U' R' U' R U R' U2 R U R'", "U R U2 R' U2 R U R' U2 R U R'", "R U R' U R U' R' U' R U' R'", "R U' R' U R U2 R' U' R U R' U R U R'", "U2 R U' R' U2 R U R' U R U' R'", "R U R' U2 R U R' U R U' R'", "U2 F' R U R' U' R' F R / R U' R U R' U R U2 R' U R'", "U2 R U R'", "U R U' R' U R U' R' U2 R U R'", "U' R U R' U' R U' R' U R U R'", "R U2 R' U' R U R' U2 R U' R'", "R U' R' U2 R U R' U2 R U R'"],
 	"UCw1":["R U' R' U R U R' U2 R U R'", "U' R U R' U R U2 R' U' R U2 R'", "U R U R' U' R U R' U2 R U' R'", "U R U R D R' U2 R D' R' U' R'", "U R' U' R U' R' U2 R U' R U R'", "R U2 R' U' R U R' U' R U2 R'", "R U R' U R U R'", "U R U R' U' R U R' U' R U2 R'"],
 	"UCcw3":["R U R' U' R U2 R' U' R U2 R'"],
 	"UCcw2":["R U2 R' U R U R' U R U' R'", "U' R U' R' U2 R U2 R'", "R U2 R' U R U2 R' U R U' R'", "U R U' R' U R U R' U2 R U' R'", "U' R U R' U2 R U' R' U R U2 R'", "R U' R' U' R U' R' U2 R U' R'"],
-	"UCcw1":["U R U2 R' U' R U' R' U' R U2 R'", "R U R' U R U' R' U R U R'", "R U' R' U2 R U' R' U2 R U' R'", "U' R U2 R' U' R U2 R' U2 R U R'", "U R U R' U2 R U' R'", "U R' U2 R U R' U R2 U' R'", "U2 R U' R' U R U R'", "U' R U' R' U R U2 R' U' R U2 R'", "R U' R' U' R U' R' U' R U2 R'", "R U2 R' U' R U2 R'", "R U2 R D R' U R D' R2", "R U' R' U R U' R' U2 R U' R'"],
-	"UCcw0":["U R U R' U R U2 R' U R U' R'", "U R U R' U' R U2 R'", "R U2 R' U2 R U' R'", "U R U R D R' U R D' R2", "U R U' R' U R U R' U' R U2 R'", "U' R U2 R' U' R U' R' U R U R'", "U2 R' U2 R U R' U R U' R U2 R'", "U2 R U R' U' R U2 R' U' R U' R'"],
+	"UCcw1":["U R U2 R' U' R U' R' U' R U2 R'", "R U R' U R U' R' U R U R'", "R U' R' U2 R U' R' U2 R U' R'", "U' R U2 R' U' R U2 R' U2 R U R'", "U R U R' U2 R U' R'", "U R' U2 R U R' U R2 U' R'", "U2 R U' R' U R U R'", "U' R U' R' U R U2 R' U' R U2 R'", "R U' R' U' R U' R' U' R U2 R'", "R U2 R' U' R U2 R'", "R U2 R D R' U R D' R2 / R U' R' U R U' R' U R U R' U R U' R'", "R U' R' U R U' R' U2 R U' R'"],
+	"UCcw0":["U R U R' U R U2 R' U R U' R'", "U R U R' U' R U2 R'", "R U2 R' U2 R U' R'", "U2 R U2 R' U' R U R' U' R U R' / U R U R D R' U R D' R2", "U R U' R' U R U R' U' R U2 R'", "U' R U2 R' U' R U' R' U R U R'", "U2 R' U2 R U R' U R U' R U2 R'", "U2 R U R' U' R U2 R' U' R U' R'"],
 	"SCw":["(R U R' U') (R U R' U) (R U' R')", "(R U2) (R' U2) (R U R' U) (R U' R')", "(R U2 R') U (R U2 R' U2) (R U R')", "U' (R U2 R' U) (R U R')", "U2 (R U' R' U') (R U R' U') (R U2 R')", "(R U2 R') U' (R U R' U R U R')", "(R U R' U') (R U R' U) (R U2 R' U') (R U2 R')", "U (R U R') U2 (R U R')"],
 	"SCcw":["(R U' R' U) (R U R' U) (R U' R')", "U2 (R U' R' U') (R U2 R')", "(R U R' U') (R U' R' U2) (R U2 R')", "U' (R U' R') U2 (R U' R')", "(R U' R' U) (R U2 R' U) (R U' R')", "U (R U R' U') R U2 (R' U' R U') R' U (R U R')", "(R U2) (R' U R U R' U) R U2 (R' U' R U' R')", "U2 (R U' R' U) (R U R' U') (R U R')"],
 };
@@ -196,7 +196,7 @@ var c20 = {
 
 
 window.algs = {
-	//"TTLL": ttll,
+	"TTLL": ttll,
     "TSLE": tsle,
 	"TTLL (from Tao Yu's trainer)": ttllTao,
     "TSLE (from Tao Yu's trainer)": tsleTao,
