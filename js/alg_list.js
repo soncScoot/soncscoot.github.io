9ac08bd33f4bd18df3d8d71cd8d01446d546e896
soncScoot <47670265+soncScoot@users.noreply.github.com>
2020-07-02 16:05:21 -0500

    OLS 3 copy fixes

--- js/alg_list.js
+++ js/alg_list.js
@@ -75,10 +75,10 @@ var c2 = {
 };
 
 var c3 = {
-	"3":["U' L U' R U2 L' U2 R' U2 L U' L'"],
-	"2":["R' U2 R U' R U2 R' U2 R' U' R2 U' R'", "U2 R U2 R' U R U2 R' U R U' R'", "R U R' U2 R U' R' U R U R'U' R U R'", "R U' R' U R U2 R' U R U2 R'", "R' U' R' U' R U R U R2 U R'", "R U2 L' U R' U' L U R U2 R'"],
-	"1":["R U R' U2 R U R' U2 R U R'", "U2 R U R' U R U' R' U R U R'", "U R' U2 R2 U R U' R' U R2 U' R' U2 R", "R U2 R' U2 R U' R' U R U2 R' U2 R U R'", "U R U2 R' U2 R U R' U2 R U' R'", "R2 U' R' U2 R2 U R' U2 R2 U' R' U2 R", "U R U2 L U' R' U L' R U' R'", "R U' R2 U2 R U R' U R2 U' R'", "R U' R' U R U R'", "R2 U2 R2 U' R2 U' R2 U' R U R'", "R2 U' R' U2 R2 U R' U2 R U R", "U' R2 U R' U R U2 R2"],
-	"0":["U' R U R' U R U2 R' U R U' R'", "U' R U' R' U R2 U R' U R U2 R2", "R U' R' U R U' R' U' R U' R'", "U R U2 R' U2 R U R' U' R U2 R'", "R U2 L' U R' U' L R U' R'", "R U2 R' U' R U' R' U' R U R'", "R2 D R' U2 R D' R' U' R'", "U2 L U' R U R' L' U' R U R'"],
+	"3":["U' L U' R U2 L' U2 R' U2 L U' L'"], 
+	"2":["R' U2 R U' R U2 R' U2 R' U' R2 U' R'", "U2 R U2 R' U R U2 R' U R U' R'", "R U R' U2 R U' R' U R U R'U' R U R'", "R U' R' U R U2 R' U R U2 R'", "R' U' R' U' R U R U R2 U R'", "R U2 L' U R' U' L U R U2 R'/R2 D R' U R D' R2'", "R U R' U2 R U R' U2 R U R'"], 
+	"1":["U2 R U R' U R U' R' U R U R'", "U R' U2 R2 U R U' R' U R2 U' R' U2 R", "R U2 R' U2 R U' R' U R U2 R' U2 R U R'", "U R U2 R' U2 R U R' U2 R U' R'", "R2 U' R' U2 R2 U R' U2 R2 U' R' U2 R", "U R U2 L U' R' U L' R U' R'", "R U' R2 U2 R U R' U R2 U' R'", "R U' R' U R U R'", "R2 U2 R2 U' R2 U' R2 U' R U R'", "R2 U' R' U2 R2 U R' U2 R U R", "U' R2 U R' U R U2 R2"], 
+	"0":["U' R U R' U R U2 R' U R U' R'", "U' R U' R' U R2 U R' U R U2 R2", "R U' R' U R U' R' U' R U' R'", "U R U2 R' U2 R U R' U' R U2 R'", "R U2 L' U R' U' L R U' R'", "R U2 R' U' R U' R' U' R U R'", "R2 D R' U2 R D' R' U' R'", "U2 L U' R U R' L' U' R U R'"], 
 };
 
 var c4 = {
