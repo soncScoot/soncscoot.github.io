7dbf50a8e4c5ed312e5612766c31eb88bb73b726
soncScoot <47670265+soncScoot@users.noreply.github.com>
2020-07-02 15:41:20 -0500

    copied OLS 1 (summer variation) fixes

--- js/alg_list.js
+++ js/alg_list.js
@@ -64,7 +64,7 @@ var sv = {
 	"3":["U2 R' D' r U2 r' D R / R U' R' U L' U2 R U' R' U2 L"],
 	"2":["L' U2 L R U' R' U L' U L / R U R' U' R U R' U R U2 R'", "R U' R' F' R U2 R' U2 R' F R / R U' L' U2 L U2 R' U2 L' U2 L / R U' L' U' L U2 R' U2 L' U L", "U2 L R U' R' U L'", "R U R'", "R U' L' U2 L U L' U L R' / R U R' U R' U2 R U R' U R", "R U R D R' U R D' R' U' R' / R U' R2 D' R U' R' D R2 U' R' / R U' R' U' R2 D R' U' R D' R2"],
 	"1":["U L' R U R' U R U' R' U2 L / R U2 R2 D' R U R' D R2 U2 R'", "R U' R D R' U2 R D' R2'", "R U2 R' U R U2 R'", "R U' R' U R' D' R U2 R' D R2 U' R'", "U2 R U' R2 D' R U' R' D R", "R U2 R' U R U' R' U R U' R' U R U2 R'/ R U' R' U' R U R' U' R U R' U' R U' R' / U' L' U R2 U' L U R' U R'", "R U' R' U' R U R' U' R U' R'", "R U' R2 U' R2 U' R2 U2 R", "L' U2 L U L' U L R U2 R'", "R U' R' U' R U' R'", "R U2 R' U' L' U R U' R' L", "R U2 R' U2 R' D' R U2 R' D R2 U' R' / R U' R' L' U R U' R' L U' R U' R'"],
-	"0":["U2 L R U' R2 U L' U' R / U2 L R U' R' U2 L' U L U2 L' / U2 R U R' U2 R U R' U2 R U2 R'", "R U2 R' U R U' R' U R U2 R'", "R U R2 U2 R U R' U R", "R U2 R D R' U' R D' R2'", "R U' R' U' R U' R2 U' R U' R' U2 R", "R U R' U R' U' R U' R' U2 R", "U' R U R D R' U R D' R2'", "R U' R' U2 L' U R U' R' L", "R U2 R' U R U' R' U R U2 R'", "R U R2 U2 R U R' U R", "R U2 R D R' U' R D' R2'", "R U' R' U' R U' R2 U' R U' R' U2 R", "R U R' U R' U' R U' R' U2 R", "U' R U R D R' U R D' R2'", "R U' R' U2 L' U R U' R' L"],
+	"0":["U2 L R U' R2 U L' U' R / U2 L R U' R' U2 L' U L U2 L' / U2 R U R' U2 R U R' U2 R U2 R'", "R U2 R' U R U' R' U R U2 R'", "R U R2 U2 R U R' U R", "R U2 R D R' U' R D' R2'", "R U' R' U' R U' R2 U' R U' R' U2 R", "R U R' U R' U' R U' R' U2 R", "U' R U R D R' U R D' R2'", "R U' R' U2 L' U R U' R' L"],
 };
 
 var c2 = {
