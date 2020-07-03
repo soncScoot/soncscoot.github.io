9aa562585ae81804d1e752f0c3ef3aa73aad0108
soncScoot <47670265+soncScoot@users.noreply.github.com>
2020-07-03 14:08:58 -0500

    OLS case 18 copy fix

--- js/alg_list.js
+++ js/alg_list.js
@@ -174,10 +174,10 @@ var c17 = {
 };
 
 var c18 = {
-	"3":["R' D' R U R' D R2 U' R'", "U R U' R' U2 R L' U2 L U L' U R' L", "U' R U R' U R2 U2 R' U' R U' R2", "U' R' D' R U' R' D R U2 R U' R'", "R U' R' U2 R' U' R2 U' R2 U2 R", "R U' R' U' R U2 R' U' R U2 R'", "U2 R U' R' U2 R U2 R'", "R U' R' U' R U' R' U2 R U' R'"],
-	"2":["U R U2 R U2 R' U' R U' R2", "U R U R' U' R U' R' U2 R U R'", "R U' R' U R U' R'", "U R U' R' U R U R' U' R U' R'", "R U R' U R U R' U2 R U2 R'", "R U' R' U' R U' R' U' R U2 R'", "R U2 R' U2 R U' R' U2 R U R'", "R U R' U2 R' U' R2 U' R2 U2 R2 U' R'", "R U2 R U2 R2 U' R2 U' R2", "R U' R' U L U' R U L' U' R'"],
-	"1":["U' R U2 R' U' R U2 R' U2 R U2 R'", "U L R U' R' U R U' R' U L'", "U' R2 U R' U R U2 R2 U2 R U R'", "U' R U' R' U2 R U' R' U R U2 R'"],
-	"0":["R U' R' L' U R U' R' L", "U' R2 U R' U R U L' U R2 U' L", "R U R' U2 L' U R U' L U' R'", "U R' U' R U' R' U2 R2 U' R' U2 R U2 R'", "R U R' U R U' R' U R2 U2 R' U' R U' R2"],
+	"3":["R' D' R U R' D R2 U' R'", "U R U' R' U2 R L' U2 L U L' U R' L", "U' R U R' U R2 U2 R' U' R U' R2", "U' R' D' R U' R' D R U2 R U' R'"], 
+	"2":["R U' R' U2 R' U' R2 U' R2 U2 R", "R U' R' U' R U2 R' U' R U2 R'", "U2 R U' R' U2 R U2 R'", "R U' R' U' R U' R' U2 R U' R'", "U R U2 R U2 R' U' R U' R2", "U R U R' U' R U' R' U2 R U R'"], 
+	"1":["R U' R' U R U' R'", "U R U' R' U R U R' U' R U' R'", "R U R' U R U R' U2 R U2 R'", "R U' R' U' R U' R' U' R U2 R'", "R U2 R' U2 R U' R' U2 R U R'", "R U R' U2 R' U' R2 U' R2 U2 R2 U' R'", "R U2 R U2 R2 U' R2 U' R2", "R U' R' U L U' R U L' U' R'", "U' R U2 R' U' R U2 R' U2 R U2 R'", "U L R U' R' U R U' R' U L'", "U' R2 U R' U R U2 R2 U2 R U R'", "U' R U' R' U2 R U' R' U R U2 R'"], 
+	"0":["R U' R' L' U R U' R' L", "U' R2 U R' U R U L' U R2 U' L", "R U R' U2 L' U R U' L U' R'", "U R' U' R U' R' U2 R2 U' R' U2 R U2 R'", "R U R' U R U' R' U R2 U2 R' U' R U' R2"], 
 };
 
 var c19 = {
